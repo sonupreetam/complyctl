726e67ee90e088d7eb48f37265e43df1229d2fee
Marcus Burghardt <maburgha@redhat.com>
2024-12-19 10:51:05 +0100

    Create config package for openscap-plugin
    Although it is not yet defined all options to be used in a plugin
    configuration file, it is already clear that a configuration file is
    necessary. This commit includes the config package with code to read
    and validate configuration while also sanitize values. Unit tests
    specific for the configuration file are not implemented now since the
    configuration file structure is still under development and
    considerable changes are expected.
    
    Signed-off-by: Marcus Burghardt <maburgha@redhat.com>

--- cmd/openscap-plugin/main.go
+++ cmd/openscap-plugin/main.go
@@ -2,8 +2,47 @@
 
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/complytime/complytime/cmd/openscap-plugin/config"
+)
+
+func parseFlags() (string, error) {
+	var configPath string
+
+	flag.StringVar(&configPath, "config", "./openscap-plugin.yml", "Path to config file")
+	flag.Parse()
+
+	configFile, err := config.SanitizeAndValidatePath(configPath, false)
+	if err != nil {
+		return "", err
+	}
+
+	return configFile, nil
+}
+
+func initializeConfig() (*config.Config, error) {
+	configFile, err := parseFlags()
+	if err != nil {
+		return nil, fmt.Errorf("error parsing flags: %w", err)
+	}
+
+	config, err := config.ReadConfig(configFile)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config from %s: %w", configFile, err)
+	}
+
+	return config, nil
+}
 
 func main() {
-	fmt.Print("OpenSCAP Plugin")
+	config, err := initializeConfig()
+	if err != nil {
+		log.Fatalf("Failed to initialize config: %v", err)
+	}
+
+	fmt.Print(config)
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/spf13/cobra v1.8.1
 	github.com/spf13/pflag v1.0.5
 	github.com/stretchr/testify v1.10.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
@@ -30,7 +31,6 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
 	google.golang.org/grpc v1.68.1 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 replace github.com/oscal-compass/compliance-to-policy-go/v2 => github.com/complytime/compliance-to-policy-go/v2 v2.0.0-20241218180447-536929e175fb
