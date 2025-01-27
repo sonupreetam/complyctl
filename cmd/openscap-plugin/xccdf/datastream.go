cd4edea9c703c8e5a29e763ad11d8147894ca499
Marcus Burghardt <maburgha@redhat.com>
2025-01-27 10:06:17 +0100

    feat: introduce functions to collect datastream variables
    Variables and their respective values are collected from profiles. In
    Datastream the profiles select options for variables and the final
    values to be used, therefore it was created another function to resolve
    these options so both alternatives can be easily used.
    
    Signed-off-by: Marcus Burghardt <maburgha@redhat.com>

--- cmd/openscap-plugin/xccdf/datastream.go
+++ cmd/openscap-plugin/xccdf/datastream.go
@@ -10,6 +10,19 @@ import (
 	"github.com/antchfx/xmlquery"
 )
 
+// The following structs can later be proposed to compliance-operator/pkg/xccdf
+type DsVariableOptions struct {
+	Selector string `xml:"selector,attr"`
+	Value    string `xml:",chardata"`
+}
+
+type DsVariables struct {
+	ID          string `xml:"idref,attr"`
+	Title       string `xml:",chardata"`
+	Description string `xml:",chardata"`
+	Options     []DsVariableOptions
+}
+
 func loadDataStream(dsPath string) (*xmlquery.Node, error) {
 	file, err := os.Open(dsPath)
 	if err != nil {
@@ -30,32 +43,59 @@ func getDsProfileID(profileId string) string {
 }
 
 func getDsElement(dsDom *xmlquery.Node, dsElement string) (*xmlquery.Node, error) {
-	// Returns nil if the element is not found
+	if dsDom == nil {
+		return nil, fmt.Errorf("dsDom is nil")
+	}
+	// NOTE: If the element is not found in dsDom, returns nil and not an error
 	return xmlquery.Query(dsDom, dsElement)
+}
+
+func getDsElementAttrValue(dsElement *xmlquery.Node, attrName string) (string, error) {
+	for _, attr := range dsElement.Attr {
+		if attr.Name.Local == attrName {
+			return dsElement.SelectAttr(attrName), nil
+		}
+	}
+	return "", fmt.Errorf("attribute not found")
+}
+
+func getDsOptionalAttrValue(dsElement *xmlquery.Node, optionalAttrName string) string {
+	attrValue, err := getDsElementAttrValue(dsElement, optionalAttrName)
+	if err != nil {
+		return ""
+	}
+	return attrValue
+}
+
+func getDsElements(dsDom *xmlquery.Node, dsElement string) ([]*xmlquery.Node, error) {
+	if dsDom == nil {
+		return nil, fmt.Errorf("dsDom is nil")
+	}
+	return xmlquery.QueryAll(dsDom, dsElement)
 }
 
 func getDsProfile(dsDom *xmlquery.Node, dsProfileID string) (*xmlquery.Node, error) {
 	return getDsElement(dsDom, fmt.Sprintf("//xccdf-1.2:Profile[@id='%s']", dsProfileID))
 }
 
-func getDsProfileTitle(dsProfile *xmlquery.Node) (*xmlquery.Node, error) {
+func getDsElementTitle(dsProfile *xmlquery.Node) (*xmlquery.Node, error) {
 	profileTitle, err := getDsElement(dsProfile, "xccdf-1.2:title")
 	if err != nil {
-		return nil, fmt.Errorf("error finding title element in profile: %s", err)
+		return nil, fmt.Errorf("error finding 'title' element: %s", err)
 	}
 	return profileTitle, nil
 }
 
-func getDsProfileDescription(dsProfile *xmlquery.Node) (*xmlquery.Node, error) {
+func getDsElementDescription(dsProfile *xmlquery.Node) (*xmlquery.Node, error) {
 	profileDescription, err := getDsElement(dsProfile, "xccdf-1.2:description")
-	if err != nil || profileDescription == nil {
-		return nil, fmt.Errorf("error finding description element in profile: %s", err)
+	if err != nil {
+		return nil, fmt.Errorf("error finding 'description' element: %s", err)
 	}
 	return profileDescription, nil
 }
 
 func populateProfileInfo(dsProfile *xmlquery.Node, parsedProfile *xccdf.ProfileElement) (*xccdf.ProfileElement, error) {
-	profileTitle, err := getDsProfileTitle(dsProfile)
+	profileTitle, err := getDsElementTitle(dsProfile)
 	if err != nil {
 		return parsedProfile, fmt.Errorf("error populating profile title: %s", err)
 	}
@@ -64,7 +104,7 @@ func populateProfileInfo(dsProfile *xmlquery.Node, parsedProfile *xccdf.ProfileE
 	}
 	if profileTitle == nil {
 		// log that profile title was not found.
-		// It is a valid case therefore but better to log it.
+		// It is a valid case but may not be expected so better to log it.
 		parsedProfile.Title.Override = false
 		parsedProfile.Title.Value = ""
 	} else {
@@ -72,7 +112,7 @@ func populateProfileInfo(dsProfile *xmlquery.Node, parsedProfile *xccdf.ProfileE
 		parsedProfile.Title.Value = profileTitle.InnerText()
 	}
 
-	profileDescription, err := getDsProfileDescription(dsProfile)
+	profileDescription, err := getDsElementDescription(dsProfile)
 	if err != nil {
 		return parsedProfile, fmt.Errorf("error populating profile description: %s", err)
 	}
@@ -81,6 +121,7 @@ func populateProfileInfo(dsProfile *xmlquery.Node, parsedProfile *xccdf.ProfileE
 	}
 	if profileDescription == nil {
 		// log that profile description was not found.
+		// It is a valid case but may not be expected so better to log it.
 		parsedProfile.Description.Override = false
 		parsedProfile.Description.Value = ""
 	} else {
@@ -91,6 +132,34 @@ func populateProfileInfo(dsProfile *xmlquery.Node, parsedProfile *xccdf.ProfileE
 	return parsedProfile, nil
 }
 
+func populateProfileVariables(dsProfile *xmlquery.Node, parsedProfile *xccdf.ProfileElement) (*xccdf.ProfileElement, error) {
+	if parsedProfile.Values == nil {
+		parsedProfile.Values = []xccdf.SetValueElement{}
+	}
+
+	profileVariables, err := getDsElements(dsProfile, "xccdf-1.2:refine-value")
+	if err != nil {
+		return parsedProfile, fmt.Errorf("error finding 'refine-value' elements in profile: %s", err)
+	}
+
+	for _, variable := range profileVariables {
+		varIdRef, err := getDsElementAttrValue(variable, "idref")
+		if err != nil {
+			return parsedProfile, fmt.Errorf("error getting value of 'idref' attribute: %s", err)
+		}
+		varSelector, err := getDsElementAttrValue(variable, "selector")
+		if err != nil {
+			return parsedProfile, fmt.Errorf("error getting value of 'selector' attribute: %s", err)
+		}
+
+		parsedProfile.Values = append(parsedProfile.Values, xccdf.SetValueElement{
+			IDRef: varIdRef,
+			Value: varSelector,
+		})
+	}
+	return parsedProfile, nil
+}
+
 func initProfile(dsProfile *xmlquery.Node, dsProfileId string) (*xccdf.ProfileElement, error) {
 	parsedProfile := new(xccdf.ProfileElement)
 	parsedProfile.ID = dsProfileId
@@ -98,6 +167,13 @@ func initProfile(dsProfile *xmlquery.Node, dsProfileId string) (*xccdf.ProfileEl
 	parsedProfile, err := populateProfileInfo(dsProfile, parsedProfile)
 	if err != nil {
 		return parsedProfile, fmt.Errorf("error populating profile title and description: %s", err)
+	}
+
+	// Here we can add the logic to populate profile rules in a separate PR
+
+	parsedProfile, err = populateProfileVariables(dsProfile, parsedProfile)
+	if err != nil {
+		return parsedProfile, fmt.Errorf("error populating profile variables: %s", err)
 	}
 
 	return parsedProfile, nil
@@ -121,21 +197,101 @@ func GetDsProfile(profileId string, dsPath string) (*xccdf.ProfileElement, error
 
 	parsedProfile, err := initProfile(dsProfile, dsProfileID)
 	if err != nil {
-		return nil, fmt.Errorf("error parsing profile %s in datastream: %s", profileId, err)
+		return nil, fmt.Errorf("error initializing a parsed profile for %s: %s", profileId, err)
 	}
 
 	return parsedProfile, nil
 }
 
 func GetDsProfileTitle(profileId string, dsPath string) (string, error) {
-	// TODO: This function can likely be removed with the introduction of the GetDsProfile function.
-	// Keeping it for now to avoid out of scope changes.
+	// TODO: This function can likely be removed with the introduction of the GetDsProfile
+	// function. Keeping it for now to avoid out of scope changes.
 	profile, err := GetDsProfile(profileId, dsPath)
 	if err != nil {
 		return "", fmt.Errorf("error processing profile %s in datastream: %s", profileId, err)
 	}
 
 	return profile.Title.Value, nil
+}
+
+func GetDsVariablesValues(dsPath string) ([]DsVariables, error) {
+	dsDom, err := loadDataStream(dsPath)
+	if err != nil {
+		return nil, fmt.Errorf("error loading datastream: %s", err)
+	}
+
+	dsVariables, err := getDsElements(dsDom, "//xccdf-1.2:Value")
+	if err != nil {
+		return nil, fmt.Errorf("error getting variables from datastream: %s", err)
+	}
+
+	dsVariablesValues := []DsVariables{}
+	for _, variable := range dsVariables {
+		varId, err := getDsElementAttrValue(variable, "id")
+		if err != nil {
+			return nil, fmt.Errorf("error getting value of 'id' attribute: %s", err)
+		}
+
+		varTitle, err := getDsElementTitle(variable)
+		if err != nil {
+			return nil, fmt.Errorf("error getting variable title: %s", err)
+		}
+
+		varDescription, err := getDsElementDescription(variable)
+		if err != nil {
+			return nil, fmt.Errorf("error getting variable description: %s", err)
+		}
+
+		varOptions, err := getDsElements(variable, "xccdf-1.2:value")
+		if err != nil {
+			return nil, fmt.Errorf("error getting variable options: %s", err)
+		}
+
+		dsVarOptions := []DsVariableOptions{}
+		for _, option := range varOptions {
+			selectorId := getDsOptionalAttrValue(option, "selector")
+			if selectorId == "" {
+				selectorId = "default"
+			}
+			selectorValue := option.InnerText()
+			dsVarOptions = append(dsVarOptions, DsVariableOptions{
+				Selector: selectorId,
+				Value:    selectorValue,
+			})
+		}
+
+		dsVariablesValues = append(dsVariablesValues, DsVariables{
+			ID:          varId,
+			Title:       varTitle.InnerText(),
+			Description: varDescription.InnerText(),
+			Options:     dsVarOptions,
+		})
+	}
+	return dsVariablesValues, nil
+}
+
+func getValueFromOption(variables []DsVariables, variableId string, selector string) (string, error) {
+	for _, variable := range variables {
+		if variable.ID == variableId {
+			for _, option := range variable.Options {
+				if option.Selector == selector {
+					return option.Value, nil
+				}
+			}
+		}
+	}
+	return "", fmt.Errorf("variable not found")
+}
+
+func ResolveDsVariableOptions(profile *xccdf.ProfileElement, variables []DsVariables) (*xccdf.ProfileElement, error) {
+	for i, value := range profile.Values {
+		resolvedValue, err := getValueFromOption(variables, value.IDRef, value.Value)
+		if err != nil {
+			return profile, fmt.Errorf("error resolving variable options: %s", err)
+		}
+		profile.Values[i].Value = resolvedValue
+	}
+	return profile, nil
 }
 
 // Getting rule information
