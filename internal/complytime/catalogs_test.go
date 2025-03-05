3118c9dd0f758ecadc733731ebee8b47e618caa7
Sophia Wang <huiwang@redhat.com>
2025-03-05 15:13:16 +0800

    Update the test case for loading catalog and profile
    Signed-off-by: Sophia Wang <huiwang@redhat.com>

---
+++ internal/complytime/catalogs_test.go
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package complytime
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadCatalogSource(t *testing.T) {
+	tests := []struct {
+		name    string
+		appDir  func() ApplicationDirectory
+		source  string
+		wantErr string
+	}{
+		{
+			name: "Valid Catalog Load",
+			appDir: func() ApplicationDirectory {
+				appDir, err := newApplicationDirectory("testdata", false)
+				require.NoError(t, err)
+				return appDir
+			},
+			source:  "file://controls/sample-catalog.json",
+			wantErr: "",
+		},
+		{
+			name: "File Does Not Exist",
+			appDir: func() ApplicationDirectory {
+				appDir, err := newApplicationDirectory("testdata", false)
+				require.NoError(t, err)
+				return appDir
+			},
+			source:  "file://nonexistent/path/catalog.json",
+			wantErr: "got path nonexistent/path/catalog.json, control source is expected to be under path testdata/complytime/controls",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := LoadProfile(tt.appDir(), tt.source)
+			if tt.wantErr != "" {
+				require.Contains(t, err.Error(), tt.wantErr)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
